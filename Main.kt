b0d8a97e5dd7cf2bff0d16407a5ec49d683e5b24
Carlos Francisco Tovar <laronsito1@gmail.com>
2026-05-26 22:03:16 -0500

    Muestra categorias disponibles al iniciar

--- Main.kt
+++ Main.kt
@@ -1,9 +1,24 @@
+import data.existeCategoria
+import data.obtenerCategorias
 import javax.swing.SwingUtilities
 import ui.TriviaVentana
 
 fun main() {
     SwingUtilities.invokeLater {
+        mostrarInformacionInicial()
+
         val ventana = TriviaVentana()
         ventana.isVisible = true
     }
+}
+
+fun mostrarInformacionInicial() {
+    val categorias = obtenerCategorias()
+
+    println("Categorias disponibles:")
+    categorias.forEach { categoria ->
+        println("- $categoria")
+    }
+
+    println("¿Existe categoria Pixar?: ${existeCategoria("Pixar")}")
 }
